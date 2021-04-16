6d4d053cf3cbdfe3e93e3fb3a5c9f69a30b34001
HelloArthur <67996100+arthurCheng2Playitloud@users.noreply.github.com>
2021-04-17 02:52:14 +0800

    init css

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,18 @@ import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 import memories from './images/memories.png';
+import useStyle from './styles';
 
 const App = () => {
+  const styles = useStyle();
+
   return (
     <Container maxWidth="lg">
-      <AppBar position="static" color="inherit">
-        <Typography variant="h2" align="center">Memories</Typography>
-        <img src={memories} alt="memories" height="300" width="300" />
+      <AppBar className={styles.appBar} position="static" color="inherit">
+        <Typography className={styles.heading} variant="h2" align="center">Memories</Typography>
+        <img className={styles.image} src={memories} alt="memories" height="60" width="60" />
       </AppBar>
+
       <Grow in>
         <Container>
           <Grid container justify="space-between" alignItems="strench" spacing={3}>
